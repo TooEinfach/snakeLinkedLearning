81afbb469d51d2746a4ff81f6e2d9cb2dd91c453
TooEinfach <whiteshadow148@gmail.com>
2024-08-23 12:18:54 -0600

    Changed shape to Turtle and added the template file.

---
+++ oop_turtle.py
@@ -0,0 +1,14 @@
+#Import Turtle Graphics module
+import turtle
+
+#Create a turtle to do your bidding
+my_tutrle = turtle.Turtle()
+my_tutrle.shape("turtle")
+my_tutrle.color('red')
+
+# Your turtle awaits your command
+my_tutrle.forward(100) # Sample command
+
+
+#This statement (or an equivalent) is nneded at the end of all your turtle programs
+turtle.done()
